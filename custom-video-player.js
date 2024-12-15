f95229d48dcdc5e7c8f397c00cd2aacd2cff741c
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2024-12-15 20:31:20 +1100

    feat: add a function to handle the changes in the video player' state
    onPlayerStateChange() handles the changes in the video player's state and perform relevant operations

--- custom-video-player.js
+++ custom-video-player.js
@@ -13,7 +13,6 @@ videoDuration.ariaLabel = `Duration: ${videoDuration.textContent.trim()}`;
 
 // select video control elements
 const videoplayBtn = document.querySelector("#video-play-btn");
-const videoPlayer = document.querySelector("#video-player");
 const videoControls = document.querySelector("#video-controls");
 const playPauseBtn = document.querySelector(".play-pause-btn");
 const seekBar = document.querySelector(".seek-bar");
@@ -36,4 +35,36 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady() {}
 
-function onPlayerStateChange(event) {}
+function onPlayerStateChange(event) {
+	if (event.data === 1) {
+		playPauseBtn.innerHTML = '<img src="pause-solid.svg" alt="">';
+		playPauseBtn.ariaPressed = "true";
+	}
+	if (event.data === 0) {
+		videoThumbnail.style.display = "block";
+		videoplayBtn.style.display = "flex";
+		videoDuration.style.display = "block";
+		videoControls.style.visibility = "hidden";
+		const videoPlayer = document.querySelector("#video-player");
+		videoPlayer.style.visibility = "hidden";
+		playPauseBtn.ariaPressed = "false";
+	}
+	if (event.data === 2 || event.data === 3 || event.data === 5) {
+		playPauseBtn.innerHTML = '<img src="play-solid.svg" alt="" />';
+		playPauseBtn.ariaPressed = "false";
+	}
+}
+
+// Utility functions
+function formatTime(seconds) {
+	const hrs = Math.floor(seconds / 3600);
+	const mins = Math.floor((seconds % 3600) / 60);
+	const secs = Math.floor(seconds % 60);
+	if (hrs > 0) {
+		return `${hrs}:${mins.toString().padStart(2, "0")}:${secs
+			.toString()
+			.padStart(2, "0")}`;
+	} else {
+		return `${mins}:${secs.toString().padStart(2, "0")}`;
+	}
+}
